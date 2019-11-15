05461a7df390294e8105ed9ff6eb140a138a168e
songzhukai <songzhukai@le.com>
2019-11-15 19:27:50 +0800

    add sort

---
+++ src/main/java/sort/BubbleSort.java
@@ -0,0 +1,37 @@
+package sort;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 6, 7, 3, 5, 6, 8, 9, 0};
+        System.out.println("before nums = " + Arrays.toString(nums));
+        bubbleSort(nums);
+        System.out.println("before nums = " + Arrays.toString(nums));
+    }
+
+    /***
+     * 冒泡排序
+     * 参考：https://www.jianshu.com/p/f31de0e89f7e
+     * step1. i 指标从 "最末端" 开始，内圈 j 持续比对，每一遍历都能得出最大值排在 "对尾"
+     * step2. j 指标持续交换，直到 i 指标至首端
+     * @param arr
+     */
+    public static void bubbleSort(int[] arr) {
+        int temp = 0;
+        for (int i = arr.length - 1; i > 0; --i) { // 每次需要排序的长度
+            for (int j = 0; j < i; ++j) { // 从第一个元素到第i个元素
+                if (arr[j] > arr[j + 1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+
+
+
+}
---
+++ src/main/java/sort/InsertionSort.java
@@ -0,0 +1,36 @@
+package sort;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 6, 7, 3, 5, 6, 8, 9, 0};
+        System.out.println("before nums = " + Arrays.toString(nums));
+        insertionSort(nums);
+        System.out.println("before nums = " + Arrays.toString(nums));
+    }
+
+    /**
+     * 插入排序
+     * 参考：https://www.jianshu.com/p/07b911fb740e
+     * step1. i 指标从 0 开始，依序 "从右方元素" 向 "左方元素" 比对
+     * step2. j 从 i 指标开始比对，仅比对 "左方元素" 做排序
+     * step3. 左方元素排序时，比对大小若不需要排序则中断 ---> 实现 "左方元素" 已排列大小
+     * step4. 依序让右方元素插入左方，直到右方元素完结
+     */
+    public static void insertionSort(int[] a) {
+        for (int i = 0; i < a.length - 1; i++) {
+            int currentItem = a[i + 1];
+            for (int j = i; j >= 0; j--) {
+                if (currentItem < a[j]) {
+                    a[j + 1] = a[j];
+                    a[j] = currentItem;
+                } else {
+                    a[j + 1] = currentItem;
+                    break;
+                }
+            }
+        }
+    }
+}
